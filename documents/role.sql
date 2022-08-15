9b4b36fb58e0d6815773281cd5a0a4dd16456198
cylon-raider <markel74@msn.com>
2022-08-14 21:05:46 -0700

    Activity 1

--- documents/role.sql
+++
@@ -1,4 +0,0 @@
-insert into ROLE(ROLE_ID, ROLE_NAME) VALUES (1, 'admin');
-insert into ROLE(ROLE_ID, ROLE_NAME) VALUES (2, 'manager');
-insert into ROLE(ROLE_ID, ROLE_NAME) VALUES (3, 'employee');
-insert into ROLE(ROLE_ID, ROLE_NAME) VALUES (4, 'user');
