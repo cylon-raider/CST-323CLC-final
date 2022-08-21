28c57670f099ec6ebde97489aed03037c3b21238
cylon-raider <markel74@msn.com>
2022-08-21 13:47:42 -0700

    Activity 3

--- src/main/java/com/gcu/clc/data/UserDataService.java
+++ src/main/java/com/gcu/clc/data/UserDataService.java
@@ -130,14 +130,4 @@ public class UserDataService implements DataAccessInterface<UserModel> {
         return loginModel;
     }
 
-    private static Connection getConnection() throws URISyntaxException, SQLException {
-        URI jdbUri = new URI(System.getenv("JAWSDB_URL"));
-
-        String username = jdbUri.getUserInfo().split(":")[0];
-        String password = jdbUri.getUserInfo().split(":")[1];
-        String port = String.valueOf(jdbUri.getPort());
-        String jdbUrl = "jdbc:mysql://" + jdbUri.getHost() + ":" + port + jdbUri.getPath();
-
-        return DriverManager.getConnection(jdbUrl, username, password);
-    }
 }
